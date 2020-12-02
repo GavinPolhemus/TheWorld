4d4c0ed644ec8011e5c9da20f52f0ff53d957b65
Hraban Ramm <hraban@fiee.net>
2020-12-02 18:30:59 +0100

    preliminary build script

---
+++ makeit.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# TODO: make paths relative
+
+cd ~/workspace/world
+
+if [ "$TEXROOT" == "" ]; then
+	#source ~/context/tex/setuptex ~/context/tex
+	#setuplmtx
+	echo TEXROOT not set!
+	exit 1
+fi
+
+VOL=$1
+MODE=$2
+
+if [ "$VOL" == "" ]; then
+	VOL=01
+fi
+
+if [ "$MODE" == "" ]; then
+	MODE=low
+fi
+
+ISODATE=`date +"%Y-%m-%d"`
+
+RESULT=TheWorld
+RESULTFILE=${RESULT}_${ISODATE}
+COPYDIR="~/Documents/Kunden/Polhemus/TheWorld/"
+#if [ ! -d $COPYDIR ]; then
+#	COPYDIR="~/Seafile/Meine Bibliothek/TheWorld"
+#fi
+context prd_volume${VOL} --result=${RESULTFILE} --mode=$MODE --autopdf=auto && cp ${RESULTFILE}.pdf $COPYDIR
